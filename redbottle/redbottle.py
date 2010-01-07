708bc4dd02a51fbca275ab36612d9eb811cc72fb
tnm <tnm@Ted-Nymans-MacBook.local>
2010-01-07 14:47:33 -0800

    some style changes; delete function

--- redbottle/redbottle.py
+++ redbottle/redbottle.py
@@ -5,43 +5,45 @@ r = redis.Redis()
 
 @route('/static/:filename')
 def static_file(filename):
-	send_file(filename, root='../redbottle/static')
+    send_file(filename, root='../redbottle/static')
 
 @route('/keyvalue/')
 @view('keys')
 def template_keyvalue():
 
-	all_keys = r.keys('*')
-	return dict(title='Key-Value Store', all_keys=all_keys)
+    all_keys = r.keys('*')
+    return dict(title='Key-Value Store', all_keys=all_keys)
 
 @route('/keyvalue/add/', method='POST')
 @view('keys')
 def template_add():
 
-	if 'key' in request.POST and 'value' in request.POST:
-		key = request.POST['key']
-		value = request.POST['value']
+    if 'key' in request.POST and 'value' in request.POST:
+        key = request.POST['key']
+        value = request.POST['value']
 
-	r.set(key,value)
-	all_keys = r.keys('*')
-	return dict(title="Key-Value Pair", key=key, value=value, all_keys=all_keys)
+        r.set(key,value)
+        all_keys = r.keys('*')
+        return dict(title="Key-Value Pair", key=key, value=value, all_keys=all_keys)
 
 @route('/keyvalue/delete/', method='POST')
 @view('keys')
 def template_delete():
 
-        if 'key_delete' in request.POST:
-                key_delete = request.POST['key_delete']
+    if 'key_delete' in request.POST:
+        key_delete = request.POST['key_delete']
 
-	all_keys = r.keys('*')
+        r.delete(key_delete)
+        all_keys = r.keys('*')
         return dict(title="Key-Value Pair", key=key_delete, all_keys=all_keys)
 
 @route('keyvalue/show/:key')
 @view('show')
 
 def template_show(key):
-	key = key
-	value = r.get(key)	
+    key = key
+    value = r.get(key)	
 	
-	return dict(title=key, key=key, value=value)	
+    return dict(title=key, key=key, value=value)	
+
 run()
