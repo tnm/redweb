2f9c7385c3ded193bb22e50f59fb8c846bc71614
tnm <tnm@Ted-Nymans-MacBook.local>
2010-01-30 16:52:48 -0800

    Adding new central.tpl template

--- redweb/redweb.py
+++ redweb/redweb.py
@@ -55,7 +55,7 @@ def template_delete_all():
 
 # SET | set a string value for a given key
 @route('/strings/set/', method='post')
-@view('strings')
+@view('central')
 def template_set():
     key = request.post.get('key', '').strip()
     value = request.post.get('value', '').strip()
@@ -68,7 +68,7 @@ def template_set():
 
 # GET | return the string value of a key
 @route('/strings/get/' method='post')
-@view('strings')
+@view('central')
 def template_get(key):
     key = request.post.get('key', '').strip()
     show_value = r.get(key)	
@@ -80,33 +80,33 @@ def template_get(key):
 
 # RPUSH | append an element to the tail of a list
 @route('/lists/rightpush/', method='post')
-@view('lists')
+@view('central')
 def template_lists_rightpush():
     key = request.post.get('key', '').strip()
-    member = request.post.get('member', '').strip()
+    element = request.post.get('element', '').strip()
 
     r.push(key,member)
     db_size = r.dbsize()
  
-    return dict(key=key, member=member, db_size=db_size)
+    return dict(key=key, element=element, db_size=db_size)
 
 
 # LPUSH | append an element to the head of a list
 @route('/lists/leftpush/', method='post')
-@view('lists')
+@view('central')
 def template_lists_leftpush():
     key = request.post.get('key', '').strip()
-    member = request.post.get('member', '').strip()
+    element = request.post.get('element', '').strip()
 
-    r.push(key,member, tail=True)
+    r.push(key,element, tail=True)
     db_size = r.dbsize()
  
-    return dict(key=key, member=member, db_size=db_size)
+    return dict(key=key, element=element, db_size=db_size)
 
 
 # LLEN | return the length of a list
 @route('/lists/length/' method='post':)
-@view('lists')
+@view('central')
 def template_lists_length():
     key = request.post.get('key', '').strip()
     length = r.llen(key)
@@ -117,7 +117,7 @@ def template_lists_length():
 
 # LRANGE | return a range of elements from a list
 @route('/lists/range/' method='post')
-@view('lists')
+@view('central')
 def template_lists_range():
     key = request.post.get('key', '').strip()
     start = request.post.get('start', '').strip()
@@ -135,7 +135,7 @@ def template_lists_range():
 
 # LPOP | return and remove the first element of a list
 @route('/lists/pop/' method='post')
-@view('lists')
+@view('central')
 def template_lists_lpop):
     key = request.post.get('key', '').strip()
     pop = r.pop(key)
@@ -146,7 +146,7 @@ def template_lists_lpop):
 
 # RPOP | return and remove the last element of a list
 @route('/lists/pop/' method='post')
-@view('lists')
+@view('central')
 def template_lists_rpop):
     key = request.post.get('key', '').strip()
     pop = r.pop(key, tail=True)
@@ -163,7 +163,7 @@ def template_lists_rpop):
 
 # SADD | add a member to a set
 @route('/sets/add/', method='post')
-@view('sets')
+@view('central')
 def template_sets_add():
     key = request.post.get('key', '').strip()
     member = request.post.get('member', '').strip()
@@ -175,7 +175,7 @@ def template_sets_add():
 
 # SREM | remove a member of a set
 @route('/sets/remove/', method='post')
-@view('sets')
+@view('central')
 def template_sets_remove():
     key = request.post.get('key', '').strip()
     member = request.post.get('member', '').strip()
@@ -190,7 +190,7 @@ def template_sets_remove():
 
 # SCARD | return the cardinality for a set
 @route('/sets/cardinality/' method='post')
-@view('sets')
+@view('central')
 def template_sets_cardinality():
     key = request.post.get('key', '').strip() 
     cardinality = r.scard(key)
@@ -208,7 +208,7 @@ def template_sets_cardinality():
 
 # SMEMBERS | return all members of a set
 @route('/sets/members/', method='post')
-@view('sets')
+@view('central')
 def template_sets_members():
     key = key
     members = r.smembers(key)
@@ -219,7 +219,7 @@ def template_sets_members():
 
 # SRANDMEMBER | return a random member of set, without removing it
 @route('/sets/random/', method='post')
-@view('sets')
+@view('central')
 def template_sets_srandom():
     key = key
     random_member = r.srandmember(key)
@@ -232,7 +232,7 @@ def template_sets_srandom():
 
 # ZADD | add a member to a sorted set
 @route('/zsets/add/', method='post')
-@view('zsets')
+@view('central')
 def template_zsets_add():
     key = request.post.get('key', '').strip()
     member = request.post.get('member', '').strip()
@@ -245,7 +245,7 @@ def template_zsets_add():
 
 # ZREM | remove a member of a sorted set
 @route('/zsets/remove/', method='post')
-@view('zsets')
+@view('central')
 def template_zsets_remove():
     key = request.post.get('key', '').strip()
     member = request.post.get('member', '').strip()
@@ -262,7 +262,7 @@ def template_zsets_remove():
 
 # ZCARD | return the cardinality for a set
 @route('/zsets/cardinality/' method='post')
-@view('zsets')
+@view('central')
 def template_zsets_cardinality():
     key = request.post.get('key', '').strip() 
     cardinality = r.zcard(key)
---
+++ redweb/views/central.tpl
@@ -0,0 +1,124 @@
+<html>
+ <head>
+
+ <title>redweb</title>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+  <script src="/static/vanadium-min.js" type="text/javascript"></script> 
+
+  <link rel="stylesheet" type="text/css" href="/static/style.css" type="text/css" />
+
+</head>
+
+<body> 
+
+<div class="wrapper">
+
+<div class="main">     
+    <h2>Strings</h2>
+
+    <form name="string_set" action="/strings/set/" method="post">
+   
+    <h3>set a string value for a given key (SET):</h3> 
+    <p>Key:</p>
+    <p><input type="text" name="key" input class=":required :only_on_submit input_form" /></p>
+    
+    <p>Value:</p>
+    <p><input type="text" name="value" input class=":required :only_on_submit input_form" /></p>
+
+    <input type="submit" class="submit_form" value="Set" />
+
+    </form>
+
+    <form name="string_get" action="/strings/get/" method="post">
+   
+    <h3>get a string value for a given key (GET):</h3> 
+    <p>Key:</p>
+    <p><input type="text" name="key" input class=":required :only_on_submit input_form" /></p>
+    
+    <p>Value:</p>
+    <p><input type="text" name="value" input class=":required :only_on_submit input_form" /></p>
+
+    <input type="submit" class="submit_form" value="Get" />
+
+    </form>
+
+    <hr />
+	
+    <h2>Lists</h2>
+
+    <form name="list_rightpush" action="/lists/rightpush/" method="post">
+    
+    <h2>append element to the tail of a list (RPUSH)</h2>
+    <p>Key:</p>
+    <p><input type="text" name="key" input class=":required :only_on_submit input_form" /></p>
+    
+    <p>Element:</p>
+    <p><input type="text" name="element" input class=":required :only_on_submit input_form" /></p>
+
+    <input type="submit" class="submit_form" value="Right Push" />
+
+    </form>
+
+    <hr />
+
+    <form name="list_leftpush" action="/lists/leftpush/" method="post">
+
+    <h2>append element to the head of a list (LPUSH)</h2>
+    <p>Key:</p>
+    <p><input type="text" name="key" input class=":required :only_on_submit input_form" /></p>
+    
+    <p>Element</p>
+    <p><input type="text" name="element" input class=":required :only_on_submit input_form" /></p>
+
+    <input type="submit" class="submit_form" value="Left Push" />
+
+    </form>
+
+    <hr />
+
+    <h2>Sets</h2>
+
+    <form name="set_add" action="/sets/add/" method="post">
+
+    <h2>add a member to a set (SADD)</h2>
+    <p>Key:</p>
+    <p><input type="text" name="key" input class=":required :only_on_submit input_form" /></p>
+    
+    <p>Member</p>
+    <p><input type="text" name="member" input class=":required :only_on_submit input_form" /></p>
+
+    <input type="submit" class="submit_form" value="Add" />
+
+    </form>
+   
+    <hr />
+
+    <form name="set_remove" action="/sets/remove/" method="post">
+
+    <h2>remove a member to a set (SREM)</h2>
+    <p>Key:</p>
+    <p><input type="text" name="key" input class=":required :only_on_submit input_form" /></p>
+    
+    <p>Member</p>
+    <p><input type="text" name="member" input class=":required :only_on_submit input_form" /></p>
+
+    <input type="submit" class="submit_form" value="Remove" />
+
+    </form>
+
+
+
+
+   
+     <p>Number of keys in store: {{db_size}}</p>
+
+
+
+</div>
+
+</div>
+
+</body>
+</html>
+
+<
